72cc975619898d23860bcad0b67ca5a3f20ce9c2
Tanzanite <brand.aftersales.service@gmail.com>
2026-08-10 01:44:54 +0800

    Relax mobile header search width

--- nuxt-i18n/app/components/SiteHeader.vue
+++ nuxt-i18n/app/components/SiteHeader.vue
@@ -1398,8 +1398,8 @@ const currentLocaleFlagSrc = computed(() => flagSrc(currentLocale.value))
 }
 
 .site-header-search-trigger--mobile {
-	width: 6.35rem;
-	min-width: 6.35rem;
+	width: 6.95rem;
+	min-width: 6.95rem;
 	height: 2.5rem;
 	justify-content: flex-start;
 	padding: 0 0.78rem !important;
@@ -1483,8 +1483,8 @@ const currentLocaleFlagSrc = computed(() => flagSrc(currentLocale.value))
 }
 
 .site-header-actions--mobile .site-header-action-cell--search {
-	flex-basis: 6.35rem;
-	width: 6.35rem;
+	flex-basis: 6.95rem;
+	width: 6.95rem;
 }
 
 .site-header-actions--mobile .site-header-language-wrapper {
